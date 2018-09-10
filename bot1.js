1425f1cc5e9323660db55df01effbdb302e1401d
wiiwiw <38856850+wiiwiw@users.noreply.github.com>
2018-09-10 00:35:22 -0700

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -1,19 +1,6 @@
 const {Client} = require('discord.js')
 const client = new Client()
-const RainClient = require("rainbow-role")
-const rain = new RainClient(prefix, roleName, size, speed, logging)
-
-//prefix (string) => البرفكس الي تبيه يكون للكوماند => default is "!"
-//roleName (string) => اسم الرول الي تبيه يكون حق ال rainbow => default is "Rainbow"
-//size (number) => كم عدد الالوان الي تبيه => default is 10
-//speed(number) => سرعة ال rainbow بالثواني => default is 60
-//logging(boolean ) => هل تبي يطلعلك بالكونسل لوق كل ما يتغير اللون؟ => default is false
- 
-//يمديك تستخدم اي من ال parmas في كودك ...
-
-rain.rainbow(client) // على حسب , اذا كنت معرفه client او bot
-
-client.on('ready',  () => {
+.on('ready',  () => {
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~Team #ALPHA  Bot~~~~~~~~~~~');
@@ -31,4 +18,53 @@ client.on("ready", () => {
 
 
 
+
+
+const config = require('./configs.json');
+
+
+const size    = config.colors;
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = '#'+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {        
+    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+        .catch(console.error);
+        
+    
+    
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
+    }
+  }
+}
+
+
+
+client.on('ready', () => {
+  console.log('Bot Is Online')
+  if(config.speed <10000){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
+  setInterval(changeColor, config.speed);
+});
 client.login(process.env.BOT_TOKEN);
