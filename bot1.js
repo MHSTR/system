7dfdabb5bb10f9dff1648a56f3d360d66a2ba727
wiiwiw <38856850+wiiwiw@users.noreply.github.com>
2018-09-09 23:46:59 -0700

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Discord = require('discord.js');
-const client = new Discord.Client();
   const moment = require('moment');
 client.on('ready',  () => {
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
